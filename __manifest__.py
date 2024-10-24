e2108c2bedc26711fc7b6fadb7c3bc00bbd5143c
christapherct <christapherct02@gmail.com>
2024-10-24 20:15:30 +0530

    Module has been initialised

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'HR Expense Request',
+    'version': '17.0',
+    'category': 'Human Resources',
+    'sequence': -100,
+    'summary': 'Module for managing employee expense requests like travel, education, and allowances.',
+    'description': """
+        This module enables employees to submit expense requests for travel, educational allowances, and other benefits.
+        The requests flow through an approval system and integrate with payroll for reimbursements.
+        """,
+    'author': 'Christapher Thomas',
+    'depends': [
+        'base', 'hr', 'hr_payroll', 'mail', 'report_xlsx'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+
+    'installable': True,
+    'application': False,
+}
